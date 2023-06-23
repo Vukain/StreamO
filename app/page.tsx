8a8355af6f9e320b8b6d5cd663967f37f92a6294
Michal Piechowiak <nilfenheim@gmail.com>
2023-06-23 20:19:25 +0200

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import styles from './page.module.sass'
+
+const Home = () => {
+  return (
+    <main className={styles.main}>
+
+    </main>
+  )
+}
+export default Home;
