e41b8b3d512ecde47a6a12d8a09cb72d7923993c
Michal Piechowiak <nilfenheim@gmail.com>
2023-06-23 21:08:22 +0200

    add: basic mongo support

--- app/api/streamers/route.ts
+++ app/api/streamers/route.ts
@@ -1,5 +1,12 @@
 import { NextResponse } from 'next/server';
+import clientPromise from "../../../lib/mongodb";
 
 export const GET = async () => {
-    return NextResponse.json({ name: 'Streamers GET' });
+    const client = await clientPromise;
+    const db = client.db("streamo");
+
+    const posts = await db.collection("streamers").find({}).limit(20).toArray();
+    // console.log(posts)
+
+    return NextResponse.json(posts);
 }
