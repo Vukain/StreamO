d2c4c32aa4523e4f87d5695f6e77fe1e51b99778
Michal Piechowiak <nilfenheim@gmail.com>
2023-06-24 13:10:05 +0200

    add: api post test

--- app/api/streamers/route.ts
+++ app/api/streamers/route.ts
@@ -17,12 +17,8 @@ export const POST = async (request: NextRequest) => {
     const client = await clientPromise;
     const db = client.db("streamo");
 
-
-    const id = 20 as unknown as ObjectId
-    const newStreamer = { _id: id, name: 'test post' }
+    const newStreamer = { _id: Date.now() as unknown as ObjectId, name: 'test post' }
     const streamers = await db.collection("streamers").insertOne(newStreamer);
-
-
 
     return NextResponse.json({ message: 'Streamer Added' });
 }
