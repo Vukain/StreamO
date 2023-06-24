edbaa2066394c1383d41afc75f7fdb281b991af2
Michal Piechowiak <nilfenheim@gmail.com>
2023-06-24 13:02:59 +0200

    add: post streamer

--- app/api/streamers/route.ts
+++ app/api/streamers/route.ts
@@ -1,12 +1,25 @@
 import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 import clientPromise from "../../../lib/mongodb";
 
 export const GET = async () => {
     const client = await clientPromise;
     const db = client.db("streamo");
 
-    const posts = await db.collection("streamers").find({}).limit(20).toArray();
+    const streamers = await db.collection("streamers").find({}).limit(20).toArray();
     // console.log(posts)
 
-    return NextResponse.json(posts);
+    return NextResponse.json(streamers);
+}
+
+export const POST = async (request: NextRequest) => {
+    const client = await clientPromise;
+    const db = client.db("streamo");
+
+    const newStreamer = { name: 'test post' }
+    const streamers = await db.collection("streamers").insertOne(newStreamer);
+
+
+
+    return NextResponse.json({ message: 'Streamer Added' });
 }
