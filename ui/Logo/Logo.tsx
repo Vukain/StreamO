8e239019eca1c5d03019d84ce67fa5ce04939f15
Michal Piechowiak <nilfenheim@gmail.com>
2023-06-25 21:59:37 +0200

    optimization: switch to nextimage

--- ui/Logo/Logo.tsx
+++ ui/Logo/Logo.tsx
@@ -1,13 +1,20 @@
+import Image from 'next/image'
+import LogoImage from '@/public/streamo-logo.png'
+
 import styles from './Logo.module.sass';
 
-import LogoImage from "../../public/streamo-logo.svg";
+// import LogoImage from "../../public/streamo-logo.svg";
 
 export const Logo: React.FC = () => {
 
     return (
         <div className={styles.wrapper}>
+            <Image className={styles.logo}
+                src={LogoImage}
+                alt="streamo logo"
+                sizes="(orientation: landscape) 20vw, 40vw"
+            />
 
-            <img className={styles.logo} src="/streamo-logo.png" alt="streamo logo" />
             {/* <LogoImage className={styles.logo} /> */}
 
         </div>
