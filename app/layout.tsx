d9b468ebe1fef4caa99982a87e87908babe1c1fa
Michal Piechowiak <nilfenheim@gmail.com>
2023-07-01 18:13:36 +0200

    add: thumb

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.sass';
+import Head from 'next/head';
 import { Lato } from 'next/font/google';
 import { Logo } from '@/ui/Logo/Logo';
-
 const lato = Lato({ weight: ['300', '400', '700'], subsets: ['latin'] });
 
 export const metadata = {
@@ -12,6 +12,11 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <meta property="og:title" content="StreamO" />
+        <meta property="og:image" content="%PUBLIC_URL%/thumb.png" />
+        <meta property="og:description" content="Designs with a twist." />
+      </head>
       <body className={lato.className}>
         <Logo />
         {children}
